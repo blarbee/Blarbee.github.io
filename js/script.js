a3bee7f164c90d16b2827eaaba92229a76e234c7
blarbee <nwl.ouachen@gmail.com>
2024-02-25 17:55:56 +0100

    tried to recreate a cool floating effect for code blocks (not quite there)

--- js/script.js
+++ js/script.js
@@ -6,13 +6,15 @@ function theme(){
     rand = Math.floor(rand).toString(16);
     let randColor = "#" + rand.padStart(6, 0).toUpperCase(); 
     root.style.setProperty("--text", randColor );
+    console.log("Text color: " + randColor);
 }
 
 const randColor = () =>  {
     // root.style.setProperty("--text", "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase());
-    root.style.setProperty("--color", "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase());
+    let color = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+    root.style.setProperty("--color", color);
+    console.log("Background color: " + color);
 }
 
-console.log(theme()); 
-console.log(randColor());
-
+theme(); 
+randColor();
