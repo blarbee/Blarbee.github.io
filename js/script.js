1e413cb6045970cf9bd88d2e22f6658bb640acaf
blarbee <nwl.ouachen@gmail.com>
2025-03-03 00:32:07 +0100

    Reset

--- js/script.js
+++ js/script.js
@@ -9,12 +9,31 @@ function theme(){
     console.log("Text color: " + randColor);
 }
 
-const randColor = () =>  {
-    // root.style.setProperty("--text", "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase());
-    let color = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-    root.style.setProperty("--color", color);
-    console.log("Background color: " + color);
+// const randColor = () =>  {
+//     // root.style.setProperty("--text", "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase());
+//     let color = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+//     root.style.setProperty("--color", color);
+//     console.log("Background color: " + color);
+// }
+
+function dark() {
+    document.body.classList.toggle("dark");
 }
 
-theme(); 
-randColor();
+var originalBG = $(".nav a").css("background-color");
+
+$('.nav li:not(".active") a').mousemove(function(e) {
+    x = e.pageX - this.offsetLeft;
+    y = e.pageY - this.offsetTop;
+    xy = x + " " + y;
+    bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", 100, from(rgba(255,255,255,0.8)), to(rgba(255,255,255,0.0))), " + originalBG;
+    bgMoz = "-moz-radial-gradient(" + x + "px " + y + "px 45deg, circle, " + lightColor + " 0%, " + originalBG + " " + gradientSize + "px)";
+
+    $(this)
+        .css({background: bgWebKit})
+        .css({background: bgMoz});
+    }).mouseleave(function() {
+    $(this).css({
+        background: originalBG
+    });
+});
