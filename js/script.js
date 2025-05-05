565db5542c1c2ef0d5d4a3e7d41eaad22a3e6811
blarbee <nwl.ouachen@gmail.com>
2025-05-05 16:05:27 +0200

    Add css for exp and edu sections

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-const root = document.documentElement;
+// const root = document.documentElement;
 
-function theme(){
-    let max = 0xFFFFFF; 
-    let rand = Math.random() * max; 
-    rand = Math.floor(rand).toString(16);
-    let randColor = "#" + rand.padStart(6, 0).toUpperCase(); 
-    root.style.setProperty("--text", randColor );
-    console.log("Text color: " + randColor);
-}
+// function theme(){
+//     let max = 0xFFFFFF; 
+//     let rand = Math.random() * max; 
+//     rand = Math.floor(rand).toString(16);
+//     let randColor = "#" + rand.padStart(6, 0).toUpperCase(); 
+//     root.style.setProperty("--text", randColor );
+//     console.log("Text color: " + randColor);
+// }
 
-function toggleTheme() {
-    document.body.classList.toggle("dark");
-    const themeIcon = document.getElementById("themeIcon");
+// function toggleTheme() {
+//     document.body.classList.toggle("dark");
+//     const themeIcon = document.getElementById("themeIcon");
 
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
-        themeIcon.src = "/assets/icons8-été-70.png";
-    } else {
-        localStorage.setItem("theme", "light");
+//     if (document.body.classList.contains("dark")) {
+//         localStorage.setItem("theme", "dark");
+//         themeIcon.src = "/assets/icons8-été-70.png";
+//     } else {
+//         localStorage.setItem("theme", "light");
 
-        themeIcon.src = "/assets/icons8_moon_light.png";
-    }
-}
+//         themeIcon.src = "/assets/icons8_moon_light.png";
+//     }
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark");
-        const themeIcon = document.getElementById("themeIcon");
-        themeIcon.src = "/assets/icons8-été-70.png";
-    }else{
-        const themeIcon = document.getElementById("themeIcon");
+// document.addEventListener("DOMContentLoaded", () => {
+//     if (localStorage.getItem("theme") === "dark") {
+//         document.body.classList.add("dark");
+//         const themeIcon = document.getElementById("themeIcon");
+//         themeIcon.src = "/assets/icons8-été-70.png";
+//     }else{
+//         const themeIcon = document.getElementById("themeIcon");
 
-        themeIcon.src = "/assets/icons8_moon_light.png";
-    }
-});
+//         themeIcon.src = "/assets/icons8_moon_light.png";
+//     }
+// });
 
-document.addEventListener('mousemove', (e) => {
-    const glow = document.getElementById('cursorGlow');
-    glow.style.top = `${e.clientY}px`;
-    glow.style.left = `${e.clientX}px`;
-});
+// document.addEventListener('mousemove', (e) => {
+//     const glow = document.getElementById('cursorGlow');
+//     glow.style.top = `${e.clientY}px`;
+//     glow.style.left = `${e.clientX}px`;
+// });
