0c0a6e0851d14221b33e953a6d5716736f335790
blarbee <nwl.ouachen@gmail.com>
2025-05-19 14:25:06 +0200

    Add Sudokus as submodule and Add css animation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 
+// =================================================================== ANIMATIONS =======================================
+const observer_fondu = new IntersectionObserver(elements => {
+    elements.forEach(element => {
+      if (element.isIntersecting) {
+        element.target.classList.add('fondu');
+      }
+    });
+  });
+  observer_fondu.observe(document.querySelector('#about_me_paragraph'));
+  observer_fondu.observe(document.querySelector('#formation_card'));
+  observer_fondu.observe(document.querySelector('.bland_container:nth-child(2)'));
 
+  const observer_right_to_left = new IntersectionObserver(elements => {
+    elements.forEach(element => {
+      if (element.isIntersecting) {
+        element.target.classList.add('from_right_to_left');
+      }
+    });
+  });
+  observer_right_to_left.observe(document.querySelector('#container_socials'));
+  observer_right_to_left.observe(document.querySelector('.bland_container:nth-child(1)'));
 
+  const observer_left_to_right = new IntersectionObserver(elements => {
+    elements.forEach(element => {
+      if (element.isIntersecting) {
+        element.target.classList.add('from_left_to_right');
+      }
+    });
+  });
 
-
-
-
-
-
-
-
-
-
+document.querySelectorAll('#container_projets>*').forEach(el => {observer_left_to_right.observe(el);});
 
 
 // const root = document.documentElement;
