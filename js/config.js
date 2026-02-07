aed4996fc2baf86e38d01f61654577f7bb4b96fb
blarbee <nwl.ouachen@gmail.com>
2026-02-07 15:37:51 +0100

    Add minor changes, Update intro

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const ActualYear = "2026";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const copyright = document.getElementById("copyright");
+    copyright.innerHTML = `© ${ActualYear} Blarbee`;
+});
